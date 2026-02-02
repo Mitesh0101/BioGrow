964126aa15a3ecad13773b5a71040e6134a075fe
Jimit Patel <jimitpatel2606@gmail.com>
2026-02-02 22:28:01 +0530

    small updates for app.py and farmer_community page

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, redirect, request, url_for, Response, render_template,session
+
+# Config means database config (username, password, db name)
 from config import Config
+
+# db is SQLAlchemy database object
 from extensions import db
 from datetime import datetime
+
+# User is table imported from models
 from models import User
+
+# To Secure Password
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
-from dotenv import load_dotenv
-load_dotenv()
 
-
+# app → your website
 app = Flask(__name__)
-app.secret_key = "Supersecret"
+
+# Connect Flask app with database
 app.config.from_object(Config)
 db.init_app(app)
+
 
 @app.route("/")
 def home():
@@ -21,12 +28,18 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
+        email = request.form.get("email")
+        password = request.form.get("password")
 
+        # User => database table
+        # User.query => asking some data from User Table
+        # User.query.filter_by(email=email) => select * from user WHERE email=input_email;
+        # .first() match the first record
         user = User.query.filter_by(email=email).first()
 
+        # check_password_hash is method of library werkzeug.security which compares hashed password stored in db table and password enterd by user
         if user and check_password_hash(user.password_hash, password):
+            # Save user info in session
             session["user_id"] = user.user_id
             session["full_name"] = user.full_name
             return redirect(url_for("dashboard"))
@@ -44,9 +57,10 @@ def profile():
 
 @app.route("/dashboard")
 def dashboard():
+    # If user not logged in then redirect to login
     if "user_id" not in session:
         return redirect(url_for("login"))
-
+    # If logged in then show dashboard
     return render_template("Dashboard/dashboard.html")
 
 
@@ -59,7 +73,7 @@ def crop_prediction():
 def farmer_community():
     return render_template("Farmer_Community/farmer_community.html")
 
-
+# Clears session (Logout User)
 @app.route("/logout")
 def logout():
     session.clear()
@@ -69,12 +83,12 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        full_name = request.form["full_name"]
-        email = request.form["email"]
-        password = request.form["password"]
-        location = request.form["location"]
-        dob = request.form["dob"]
-        mobile = request.form["mobile"]
+        full_name = request.form.get("full_name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        location = request.form.get("location")
+        dob = request.form.get("dob")
+        mobile = request.form.get("mobile")
 
         if User.query.filter_by(email=email).first():
             return "Email already registered"
@@ -92,10 +106,10 @@ def register():
             created_at=datetime.now()
         )
 
-
+        # Add User in db table
         db.session.add(user)
         db.session.commit()
-
+        
         return redirect(url_for("login"))
     return render_template("Login/login.html")
 
